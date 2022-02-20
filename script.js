9a2732bdbf2dcc2a1df7df61a5eec010d4148697
Dubjay18 <jejeniyi7@gmail.com>
2022-02-20 23:19:09 +0100

    third commit

--- script.js
+++ script.js
@@ -9,10 +9,12 @@ function scrollFunction() {
     document.getElementById("navbar").style.zIndex = "100";
     document.getElementById("logo").style.fontSize = "25px";
     document.getElementById("navbar").style.marginTop = "-76px";
+    document.getElementById("navbar").style.transition = "0s";
   } else {
     document.getElementById("navbar").style.marginTop = "0px";
     document.getElementById("navbar").style.width = "100%";
     document.getElementById("logo").style.fontSize = "1.25rem";
     document.getElementById("navbar").style.position = "relative";
+    document.getElementById("navbar").style.transition = "0.7s";
   }
 }
